c433e8c6d54970cc981d81fc179e80ee7a33729c
supriya <supriya@supriyas-MacBook-Air.local>
2023-05-16 13:22:08 +0530

    bug fix

--- sumFunction.py
+++ sumFunction.py
@@ -2,13 +2,3 @@ x = 2
 y = 10
 z = x * y
 print(z)
-
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
